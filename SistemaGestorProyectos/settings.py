3a2955d9ceab133bb85aad8dc294d8af8af108b3
Alejandro347 <alemonsan05@gmail.com>
2023-10-27 03:43:37 -0600

    conexion a MariaDB

--- SistemaGestorProyectos/settings.py
+++ SistemaGestorProyectos/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'SistemaGestorProyectos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema_gestor_de_proyectos',
+        'USER': 'usuario_too',
+        'PASSWORD': 'too115',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
