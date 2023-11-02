26f22fdd00af457092ff10566191539f44c62a39
Danield05 <joseaquino971@gmail.com>
2023-11-01 21:02:17 -0600

    cambios en url

--- SistemaGestorProyectos/urls.py
+++ SistemaGestorProyectos/urls.py
@@ -18,6 +18,26 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path,include
 from login import views as login_views
+from administracionProyecto import views as administracionProyecto_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path("",include('login.urls')),
+    path('gestionar_proyecto/', administracionProyecto_views.gestionar, name='gestionar_proyecto'),
+    path('gestionar_proyecto/crear_proyecto/', administracionProyecto_views.crearProyecto, name='crear_proyecto'),
+    path('gestionar_proyecto/proyecto/<int:id>/', administracionProyecto_views.proyecto, name='proyecto'),
+    path('gestionar_proyecto/editarProyecto/<int:id>/', administracionProyecto_views.editarProyecto, name='editarProyecto'),
+    path('gestionar_proyecto/eliminarProyecto/<int:id>/', administracionProyecto_views.eliminarProyecto, name='eliminarProyecto'),
+    path('gestionar_proyecto/proyecto/<int:id>/aprobarProyecto/', administracionProyecto_views.aprobarProyecto, name='aprobarProyecto'),
+    path('listaTarea/', administracionProyecto_views.listaTarea, name='listaTarea'),
+    path('listaTarea/<int:id>/', administracionProyecto_views.listaTarea, name='listaTarea'),
+    path('listaTarea/crearTarea/', administracionProyecto_views.crearTarea, name='crearTarea'),
+    path('listaTarea/editarTarea/<int:id>/', administracionProyecto_views.editarTarea, name='editarTarea'),
+    path('listaTarea/eliminarTarea/<int:id>/', administracionProyecto_views.eliminarTarea, name='eliminarTarea'),
+    path('listaRecurso/', administracionProyecto_views.listaRecurso, name = 'listaRecurso'),
+    path('listaRecurso/<int:id>/', administracionProyecto_views.listaRecurso, name = 'listaRecurso'),
+    path('listaRecurso/agregarRecurso/', administracionProyecto_views.agregarRecurso, name = 'agregarRecurso'),
+    path('listaRecurso/editarRecurso/<int:id>/', administracionProyecto_views.editarRecurso, name = 'editarRecurso'),
+    path('listaRecurso/eliminarRecurso/<int:id>/', administracionProyecto_views.eliminarRecurso, name = 'eliminarRecurso'),
+]
