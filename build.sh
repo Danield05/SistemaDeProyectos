278cd131e664e0f60ce1b485ff959f5e931de161
Alejandro347 <alemonsan05@gmail.com>
2023-11-03 23:21:27 -0600

    agregando migraciones

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
+python manage.py migrate administracionProyecto
+python manage.py migrate login
 python manage.py migrate auth
 python manage.py migrate
 python manage.py collectstatic --no-input
