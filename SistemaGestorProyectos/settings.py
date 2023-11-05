40aae7ba5c09bd45c94242bf0345368a7fc30de1
Alejandro347 <alemonsan05@gmail.com>
2023-11-04 22:53:12 -0600

    maria

--- SistemaGestorProyectos/settings.py
+++ SistemaGestorProyectos/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,14 +96,16 @@ WSGI_APPLICATION = 'SistemaGestorProyectos.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sistema_gestor_de_proyectos',
-        'USER': 'usuario_too',
-        'PASSWORD': 'too115',
-        'HOST': 'localhost',
-        'PORT': 3306,
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'HOST': env('STACKHERO_MARIADB_HOST'),
+    'OPTIONS': {
+      'ssl_mode': 'REQUIRED',
+    },
+    'NAME': 'mariaDB',
+    'USER': 'root',
+    'PASSWORD': env('STACKHERO_MARIADB_ROOT_PASSWORD')
+  }
 }
 
 
