90e359a6d1eae9f0c7b0f92f5b1cb26cdefcdaa8
Alejandro347 <alemonsan05@gmail.com>
2023-11-05 01:05:27 -0600

    agregando roles

--- login/models.py
+++ login/models.py
@@ -1,12 +1,12 @@
-# from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group
 
-# #Crear grupos/roles
-# admin_group, created = Group.objects.get_or_create(name='Administrador')
-# sup_group, created = Group.objects.get_or_create(name='Supervisor')
-# emp_group, created = Group.objects.get_or_create(name='Empleado Formulador de proyectos')
-# inv_group, created = Group.objects.get_or_create(name='Inversor')
-# #documentacion
-# #https://vegibit.com/how-to-create-and-use-django-groups-for-user-management/
+#Crear grupos/roles
+admin_group, created = Group.objects.get_or_create(name='Administrador')
+sup_group, created = Group.objects.get_or_create(name='Supervisor')
+emp_group, created = Group.objects.get_or_create(name='Empleado Formulador de proyectos')
+inv_group, created = Group.objects.get_or_create(name='Inversor')
+#documentacion
+#https://vegibit.com/how-to-create-and-use-django-groups-for-user-management/
 
 from django.db import models
 from django.contrib.auth.models import User
